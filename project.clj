2ceeceb98f00e00050b8d81b51f53a23ed23979b
Cameron Barre <cbarre@bcvevolve.com>
2017-02-27 21:08:24 -0800

    Update project.clj
    Remove proto-repl dependency, add descriptions and licensing info.

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject clj-idope "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject clj-idope "0.1.0"
+  :description "A low level Clojure API client for the IDope torrent site API."
+  :url "https://github.com/cjbarre/clj-idope/"
+  :license {:name "The unlicense"
+            :url "http://unlicense.org/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [proto-repl "0.3.1"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-time "0.13.0"]
-                 [clj-http "2.3.0"]]
-  :aot :all)
+                 [clj-http "2.3.0"]])
